fddbb99bf6757937f317b1c935626935562763b0
Lexie_Murphy <Lexie.Murphy88@gmail.com>
2018-12-12 00:34:44 -0600

    css styling and added jumbotron with different play button

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,82 +1,80 @@
-// Creates an array that lists out all of the options (numbers).
-var computerChoices = [1, 4, 5, 10];
-
-// Creating variables to hold the number of wins, losses, and guessesLeft. They start at 9.
+// Create variables to hold number values.
 var wins = 0;
 var losses = 0;
-var guessesLeft = 9;
+var userChoice = 0;
+var totalScoreNumber = 0;
+var computerChoice = 0;
+var crystal1 = 0;
+var crystal2 = 0;
+var crystal3 = 0;
+var crystal4 = 0;
+var playAgain = false;
 
-// Create variables that hold references to the places in the HTML where we want to display things.
-var directionsText = document.getElementById("directions-text");
-var userChoiceText = document.getElementById("userchoice-text");
-var computerChoiceText = document.getElementById("computerchoice-text");
+
+// Create variables that hold references to the places in the HTML where we want to display the results/text.
+var userChoiceText = document.getElementById("userChoice-number");
+var computerChoiceText = document.getElementById("computerChoice-text");
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
-var guessesLeftText = document.getElementById("guessesLeft-text");
+var totalScoreNumberText = document.getElementById("totalScoreNumber-text");
 
-var computerGuess = "";
+// Display what you want the text to say/contain.
+winsText.textContent = "Wins: " + wins;
+lossesText.textContent = "Losses: " + losses;
+computerChoiceText.textContent = "Computer Choice: " + computerChoice;
+totalScoreNumberText.textContent = totalScoreNumber;
 
 
-var computerGuessFunction = function() {
-    computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+// Build out your on click functions.
+window.onload = function() {
+ $("#letsPlay").on("click", startGame);
+ $("#crystal1").on("click", crystal1);
+ $("#crystal2").on("click", crystal2);
+ $("#crystal3").on("click", crystal3);
+ $("#crystal4").on("click", crystal4);
 };
 
+// Run function to randomly assign values to each crystal and hide lets play button when let's plan is clicked.
+function startGame() {
+  crystal1 = (Math.floor(Math.random() * 10) + 1);
+  crystal2 = (Math.floor(Math.random() * 10) + 1);
+  crystal3 = (Math.floor(Math.random() * 10) + 1);
+  crystal4 = (Math.floor(Math.random() * 10) + 1);
+  playAgain = false;
+  }
 
-// This function is run whenever the user presses a key.
-document.onkeyup = function(event) {
-    console.log(guessesLeft, guessesLeftText);
+// When a crystal is clicked, run function to add value to totalScoreNumber.
+function crystal1() {
+  totalScoreNumber = (totalScoreNumber + crystal1);
+}
+function crystal2() {
+  totalScoreNumber = (totalScoreNumber + crystal2);
+}
+function crystal3() {
+  totalScoreNumber = (totalScoreNumber + crystal3);
+}
+function crystal4() {
+  totalScoreNumber = (totalScoreNumber + crystal4);
+}
 
-  // Determines which key was pressed.
-  var userGuess = event.key;
+// If totalScoreNumber is greater than computerChoice then alert lost, add a loss, and confirm if want to play again.
+if (totalScoreNumber > computerChoice) {
+  alert("You lost!");
+  loss++;
+  confirm("Do you want to play again?", playAgain);
+}
 
-  // If computerGuess is blank, set it.
-    // Randomly chooses a choice from the options array. This is the Computer's guess.
-    if (computerGuess === "") {
-        computerGuessFunction();
-    }
-    
+// If totalScoreNumber is equal to computerChoice then alert won, add a win, and ask if want to play again.
+if (totalScoreNumber!== 0 && totalScoreNumber === computerChoice) {
+  alert("You won!");
+  wins++;
+  confirm("Do you want to play again?", playAgain);
+}
 
-  // Determine the outcome of the game (win/loss/guessesLeft), and increments the appropriate number
-  if (userGuess === computerGuess && userGuess !== "") {
-      wins++;
-      guessesLeft= 9;
-      userGuess = "";
-      computerGuessFunction();
+// if want to play again is true - reset values.
+if (playAgain === true) {
+  var userChoice = 0;
+  var totalScoreNumber = 0;
+  var computerChoice = 0;
+};
 
-    } else if (guessesLeft > 0) {
-      guessesLeft--;
-
-    } else {
-        console.log("reset");
-      losses++;
-      guessesLeft = 9;
-      computerChoiceText.textContent = "Computer's Answer Was: ";
-      userChoiceText.textContent = "Your Guesses so far: " + userGuess;
-      computerGuessFunction();
-    //   userGuess = "";
-    }
-
-    // Hide the directions
-    directionsText.textContent = "";
-
-    // Display the user and computer guesses, and wins/losses/guessesLeft.
-
-    winsText.textContent = "Wins: " + wins;
-    lossesText.textContent = "Losses: " + losses;
-    guessesLeftText.textContent = "Guesses Left: " + guessesLeft;
-    
-
-    // Display the string of your guesses so far.
-    if (guessesLeft >= 8 && guessesLeft !== 0) {
-        userChoiceText.textContent = "Your Guesses so far: " + userGuess;
-
-    } else {
-    userChoiceText.textContent += ", " + userGuess;
-    }
-
-    // If out of guesses, show the computer's guess.
-    if (guessesLeft === 0) {
-    computerChoiceText.textContent = "Computer's Answer Was: " + computerGuess;
-    computerGuessFunction();
-    }
-  };
